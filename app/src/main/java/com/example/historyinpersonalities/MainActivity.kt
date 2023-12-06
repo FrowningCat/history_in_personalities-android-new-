b6c02225a6f8852fbe243f87d90bf2cc14a21bfa
FrowningCat <vsavinkov60@gmail.com>
2023-12-06 13:48:59 +0300

    add links

--- app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
+++ app/src/main/java/com/example/historyinpersonalities/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.historyinpersonalities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -12,13 +13,43 @@ class MainActivity : AppCompatActivity() {
 
         var linkToCategory: TextView = findViewById(R.id.linkToCat)
         var linkToPeten: TextView = findViewById(R.id.linkToPeten)
+        var linkToError1: ImageView = findViewById(R.id.linkToError1)
+        var linkToError2: ImageView = findViewById(R.id.linkToError2)
+        var linkToError3: ImageView = findViewById(R.id.linkToError3)
+        var linkToError4: ImageView = findViewById(R.id.linkToError4)
+        var linkToError5: ImageView = findViewById(R.id.linkToError5)
+        var linkToError6: ImageView = findViewById(R.id.linkToError6)
 
         linkToPeten.setOnClickListener{
-            val intent = Intent(this, error::class.java)
+            val intent = Intent(this, peten::class.java)
             startActivity(intent)
         }
         linkToCategory.setOnClickListener{
             val intent = Intent(this, article::class.java)
+            startActivity(intent)
+        }
+        linkToError1.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError2.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError3.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError4.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError5.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError6.setOnClickListener{
+            val intent = Intent(this, error::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/historyinpersonalities/article.kt
+++ app/src/main/java/com/example/historyinpersonalities/article.kt
@@ -1,11 +1,56 @@
 package com.example.historyinpersonalities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 
 class article : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
+
+        var linkToPeten2: ImageView = findViewById(R.id.linkToPeten2)
+        var linkToError7: ImageView = findViewById(R.id.linkToError7)
+        var linkToError8: ImageView = findViewById(R.id.linkToError8)
+        var linkToError9: ImageView = findViewById(R.id.linkToError9)
+        var linkToError10: ImageView = findViewById(R.id.linkToError10)
+        var linkToError11: ImageView = findViewById(R.id.linkToError11)
+        var linkToError12: ImageView = findViewById(R.id.linkToError12)
+        var linkToError13: ImageView = findViewById(R.id.linkToError13)
+
+        linkToPeten2.setOnClickListener{
+            val intent = Intent(this, peten::class.java)
+            startActivity(intent)
+        }
+        linkToError7.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError8.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError9.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError10.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError11.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError12.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
+        linkToError13.setOnClickListener{
+            val intent = Intent(this, error::class.java)
+            startActivity(intent)
+        }
     }
 }
